8ef3c8cc8e678883eeae74d704b4dea3d0d6ae01
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-26 02:36:34 +0300

    Variable initialization and formatting into the string

--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 int main (void) {
-    printf("Hello world\n");
+    char helloWorld[] = "Hello world"; //do not use string types in C instead use char as an array
+    char name[] = "Geofrey";
+    int age = 22;
+    printf("%s, My name is %s and I am %i years old\n", helloWorld,name,age);
     return 0;
 }
