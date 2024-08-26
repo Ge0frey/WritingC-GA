05a3db263769775780228bfc02c2e008410c80da
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-26 03:13:29 +0300

    Use fgets to get string values

--- userInput.c
+++ userInput.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main (void) {
+    char name[20];
+    printf("What is your name: ");
+    fgets(name,20,stdin);
+    printf("My name is %s", name);
     int age;
     printf("What is your age: \n");
     scanf("%d", &age);
