2b67ee3780f8319d8926b7220a427b8b29de93a0
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-26 07:16:53 +0300

    Feat:Trial limits

--- guessinggame.c
+++ guessinggame.c
@@ -3,10 +3,24 @@
 int main () {
     int secretNumber = 4;
     int guess;
+    int guessCount = 0;
+    int guessLimit = 3; 
+    int outofGuesses = 0;
 
-    while (guess != secretNumber) {
-        printf("Enter number: ");
-        scanf("%d", &guess);
+    while (guess != secretNumber && outofGuesses == 0) {
+        if (guessCount < guessLimit) {
+            printf("Enter number: ");
+            scanf("%d", &guess);
+            guessCount++;
+        } else {
+            outofGuesses = 1;
+        }
     }
-    printf("You win\n");
+    if(outofGuesses == 1) {
+        printf("Out of guesses\n");
+    } else {
+        printf("You win\n");
+    }
 }
+
+
