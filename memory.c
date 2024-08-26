9c99b84a5a7e6221f0f22cadaed944f16dd236c8
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-26 08:01:10 +0300

    A pointer is a 'data type' that points to the memory address

--- memory.c
+++ memory.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main () {
-    int age = 30;
+    int age = 22;
+    int * PAge = &age; // pointer variable which is storing the memory address of the integer age variable
+
     double gpa = 3.2;
+    double * pGpa = &gpa; //pointer variable storing memory address of the double gpa variable 
+
     char grade = 'A';
+    char * pGrade = &grade; //pointer variable storing the memory address of grade characters
 
     printf("Age: %p\n gpa: %p\n grade: %p\n", &age, &gpa, &grade);
 }
+
+//A pointer is a 'data type' that points to the memory address
